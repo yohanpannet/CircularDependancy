5000d3206422f860a140b932fea286087337d166
yohan-pannet-bbo <yohan@bridgebase.com>
2023-11-16 18:08:00 +0200

    feat: no more work

--- test/dependance.test.ts
+++ test/dependance.test.ts
@@ -25,60 +25,60 @@ describe('Une dependance circulaire', () => {
   })
 });
 
-xdescribe('deux dependances circulaire', () => {
+describe('deux dependances circulaire', () => {
   it('sont égales si les ont la même sequence', () => {
-    let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-    let dp2: DependanceCirculaire = new DependanceCirculaire("c -> a -> b -> c");
+    let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+    let dp2: DependanceCirculaire = new DependanceCirculaire("c > a > b > c");
     dp.equals(dp2).should.be.true();
   })
   it('sont différentes si les n\'ont pas la même sequence', () => {
-    let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-    let dp2: DependanceCirculaire = new DependanceCirculaire("c -> a -> d -> c");
+    let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+    let dp2: DependanceCirculaire = new DependanceCirculaire("c > a > d > c");
     dp.equals(dp2).should.be.false();
   })
   it('sont différentes si les n\'ont pas la même sequence', () => {
-    let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-    let dp2: DependanceCirculaire = new DependanceCirculaire("a -> c -> b -> a");
+    let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+    let dp2: DependanceCirculaire = new DependanceCirculaire("a > c > b > a");
     dp.equals(dp2).should.be.false();
   })
   it('sont différentes si les n\'ont pas la même sequence', () => {
-    let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-    let dp2: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> d -> a");
+    let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+    let dp2: DependanceCirculaire = new DependanceCirculaire("a > b > c > d > a");
     dp.equals(dp2).should.be.false();
   })
 })
 
-xdescribe('une dependances circulaire', () => {
+describe('une dependances circulaire', () => {
   describe('est une sous sequence dans les cas suivants', () => {
 
-    it('a -> b -> c -> a dans z -> a -> b -> c -> z', () => {
-      let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-      let dp2: DependanceCirculaire = new DependanceCirculaire("z -> a -> b -> c -> z");
+    it('a > b > c > a dans z > a > b > c > z', () => {
+      let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+      let dp2: DependanceCirculaire = new DependanceCirculaire("z > a > b > c > z");
       dp.isSubSequenceOf(dp2).should.be.true();
     })
-    it('a -> b -> c -> a dans z -> c -> a -> b -> z', () => {
-      let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-      let dp2: DependanceCirculaire = new DependanceCirculaire("z -> c -> a -> b -> z");
+    it('a > b > c > a dans z > c > a > b > z', () => {
+      let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+      let dp2: DependanceCirculaire = new DependanceCirculaire("z > c > a > b > z");
       dp.isSubSequenceOf(dp2).should.be.true();
     })
-    it('a -> b -> c -> a dans b -> c -> d -> a -> b', () => {
-      let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-      let dp2: DependanceCirculaire = new DependanceCirculaire("b -> c -> d -> a -> b");
+    it('a > b > c > a dans b > c > d > a > b', () => {
+      let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+      let dp2: DependanceCirculaire = new DependanceCirculaire("b > c > d > a > b");
       dp.isSubSequenceOf(dp2).should.be.true();
     })
   })
 
   describe('n\'est pas une sous sequence dans les cas suivants', () => {
 
-    it('a -> b -> c -> a dans z -> a -> b -> d -> z', () => {
-      let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-      let dp2: DependanceCirculaire = new DependanceCirculaire("z -> a -> b -> d -> z");
+    it('a > b > c > a dans z > a > b > d > z', () => {
+      let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+      let dp2: DependanceCirculaire = new DependanceCirculaire("z > a > b > d > z");
       dp.isSubSequenceOf(dp2).should.be.false();
     })
 
-    it('a -> b -> c -> a dans z -> a -> c -> b -> z', () => {
-      let dp: DependanceCirculaire = new DependanceCirculaire("a -> b -> c -> a");
-      let dp2: DependanceCirculaire = new DependanceCirculaire("z -> a -> c -> b -> z");
+    it('a > b > c > a dans z > a > c > b > z', () => {
+      let dp: DependanceCirculaire = new DependanceCirculaire("a > b > c > a");
+      let dp2: DependanceCirculaire = new DependanceCirculaire("z > a > c > b > z");
       dp.isSubSequenceOf(dp2).should.be.false();
     })
   })
